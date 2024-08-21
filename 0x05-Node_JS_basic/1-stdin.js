c83de25cc64753ed6e3eaf4c300d193bdb399591
Henzz <khenok53@gmail.com>
2024-08-22 01:13:29 +0300

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,10 +3,14 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-const greetings = "Welcome to Holberton School, what is your name?";
+const greetings = "Welcome to Holberton School, what is your name?\n";
 
-console.log(greetings);
-rl.question(``, (name) => {
+// console.log(greetings);
+rl.question(greetings, (name) => {
   console.log(`Your name is: ${name}`);
   rl.close();
+});
+
+rl.on("close", () => {
+  console.log("This important software is now closing");
 });
