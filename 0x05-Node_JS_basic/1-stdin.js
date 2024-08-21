070ecdbe7f0bb0902f6ce5ffcf2b84a61f69a7e7
Henzz <khenok53@gmail.com>
2024-08-22 01:47:08 +0300

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,15 @@
-const greetings = "Welcome to Holberton School, what is your name?\n";
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-
-process.stdin.on("readable", function () {
+process.stdin.on('readable', function () {
   var name = process.stdin.read();
   if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}\n`);
   }
   process.exit(0);
 });
 
-process.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
   process.exit(0);
 });
 
