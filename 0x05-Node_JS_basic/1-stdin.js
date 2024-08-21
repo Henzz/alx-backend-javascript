4df508fd46387587dd32e3ca9a5cceef4e3cf5e5
Henzz <khenok53@gmail.com>
2024-08-22 01:49:55 +0300

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write("Welcome to Holberton School, what is your name?");
 
 process.stdin.on("readable", function () {
   var name = process.stdin.read();
   if (name !== null) {
-    console.log(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}`);
   }
   process.exit(0);
 });
 
 process.on("end", () => {
-  console.log("This important software is now closing");
+  process.stdout.write("This important software is now closing");
   process.exit(0);
 });
 
