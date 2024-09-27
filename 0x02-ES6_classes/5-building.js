e652639d500ba8547258ec8a12a1c89a0ae1cafa
Henzz <khenok53@gmail.com>
2024-09-27 16:55:50 +0300

    task 5 fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be of type number');
-    }
     this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  static evacuationWarningMessage() {
-    // Check for missing evacuationWarningMessage in subclasses
-    if (!this.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+  set sqft(value) {
+    this._sqft = value;
   }
 }
